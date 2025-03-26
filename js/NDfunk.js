5f1697c067af2ecc1519bfcfa5c3bdbe5cfe7d37
Robotas <zilvism@gmail.com>
2025-03-26 11:56:22 +0200

    initial commit

---
+++ js/NDfunk.js
@@ -0,0 +1,53 @@
+
+function tusciaFunkcija() {
+return
+   { console.log(false)}
+   }
+
+
+function skaitmenys(a) {}
+    if (isNaN(a)) {    
+    console.log('Pateikta netinkamo tipo reiksme')
+} 
+    else {
+    const ilgis = a.length;
+    }
+    {
+        console.log('Skaitmenu kiekis skaiciuje ' + ilgis);
+    }
+    
+
+
+skaitmenys(5)
+skaitmenys(781)
+skaitmenys(37060123456)
+skaitmenys('true')
+skaitmenys('asd')
+skaitmenys('NaN')
+
+
+
+function didziausiasSkaiciusSarase(skaiciai) {
+let mas = Array.isArray(skaiciai);
+if (mas == false) ; 
+{console.log('Pateikta netinkamo tipo reiksme.')}
+if (skaiciai == ' ');
+{console.log('Pateiktas sarasas negali buti tuscias.')}
+}
+let n = skaiciai.length;
+let max = skaiciai[0]; {
+if (skaiciai[i] > max);
+for (let i=1, i<n, i++) 
+    max = skaiciai[i];}
+{console.log(max)}
+
+
+didziausiasSkaiciusSarase([1]) 
+didziausiasSkaiciusSarase([1, 2, 3])
+didziausiasSkaiciusSarase([ -5, 78, 14, 0, 18])
+didziausiasSkaiciusSarase([69, 69, 69, 69, 66])
+didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8 ])
+didziausiasSkaiciusSarase(['pomidoras']);
+didziausiasSkaiciusSarase[('')]
+
+
