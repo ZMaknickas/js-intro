10011fdb6bcd331cd509a6f4fd10c565a66909ff
Robotas <zilvism@gmail.com>
2025-03-26 10:04:11 +0200

    initial commit

---
+++ js/filmai.js
@@ -0,0 +1,22 @@
+const filmas1 = ['Operacija fortuna', 'komedija', 9];
+let filmas2 = ['Maverikas', 'veiksmo', 8];
+let filmas3 = ['Pjuklas', 'fantastinis', 6];
+let filmas4 = ['Barselona', 'drama', 7];
+let filmas5 = ['Hateful8', 'veiksmo', 10];
+
+function xx(kino)
+{
+   if ( kino[2] >= 8  && kino[1] === ('veiksmo')) 
+   { return console.log('Filmo pavadinimas' + kino[0] + kino[2])}
+
+
+}
+
+ 
+
+
+xx(filmas1)
+xx(filmas2)
+xx(filmas3)
+xx(filmas4)
+xx(filmas5)
