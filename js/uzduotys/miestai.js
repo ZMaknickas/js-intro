8d47e1be7e0c729be0a74c3a82fc2014ffe62292
Muncle <zilvism@gmail.com>
2025-04-02 13:54:00 +0300

    initial commit

--- js/uzduotys/miestai.js
+++ js/uzduotys/miestai.js
@@ -21,4 +21,16 @@ console.log(kelione(kelias[i], kelias[i+1], 1.5))}
 
 //Parašykite funkciją, kuri pasakytų, ar spėtume pasiekti norimą miestą per x
 //valandų ir kokiu greičiu turėtume važiuoti? (padarykite limitą 120 km/h)
+const kelias2 = ['Marijampole', 130, 'Utena', 100, 'Druskininkai', 137, 'Kaunas', 89, 'Molėtai', 66, 'Palanga', 339, 'Birzai',
+    212, 'Trakai', 30, 'Ignalina', 117, 'Rudamina', 19];
 
+function spetume(miestas, atstumas, norimasl) {
+    laikasmin = atstumas/120;
+    if (laikasmin<=norimasl)
+    return 'Pasiektume miesta '+ miestas;
+    else 
+    return miestas + ' nepasieksime vaziuodami 120km/h per '+norimasl+' h';
+}
+
+for (let i=0; i<=19; i+=2) {
+    console.log(spetume(kelias2[i], kelias2[i+1], 1.5))}
