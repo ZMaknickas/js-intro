5838063eb263535c6d71d2e7a2f93c59f2765a0a
Muncle <zilvism@gmail.com>
2025-04-02 09:47:38 +0300

    initial commit

--- js/11-method-string.js
+++ js/11-method-string.js
@@ -38,11 +38,67 @@ console.log('Pomidoras'.indexOf('x'));
 console.log('Pomidoras'.indexOf('xXx'));
 console.log('Pomidoras'.indexOf('mido'));
 
-console.lod('Pomidoras'.indexOf('o'));
-console.lod('Pomidoras'.indexOf('o', 0));
-console.lod('Pomidoras'.indexOf('o', 1));
-console.lod('Pomidoras'.indexOf('o', 2));
+console.log('Pomidoras'.indexOf('o'));
+console.log('Pomidoras'.indexOf('o', 0));
+console.log('Pomidoras'.indexOf('o', 1));
+console.log('Pomidoras'.indexOf('o', 2));
 
 const k = 'kulverstukas';
 console.log(k.indexOf('u', k.indexOf('u')+1));
 
+//'asfd'. meta sarasha metodu su stringais...
+
+// repeat tekstas.repeat 
+
+console.clear();
+console.log('n\repeat');
+console.log('a');
+
+
+const symbol = 'm';
+const megaSymbol = symbol.repeat(10);
+console.log(symbol);
+console.log(megaSymbol)
+
+function stringRepeat(text, count) {
+    let result = '';
+    for (let i= 0; i<Math.floor(count); i++) {
+        result +=text;
+    }
+    return result;
+}
+console.log(stringRepeat('rikiki', 3));
+
+// replace
+console.clear();
+console.log('\nreplace');
+
+const summer='vasara'.replace('a', '-')
+console.log('vasara'.replace('a','A'));
+
+// chain (keletas kartu replace)
+
+const summer2= 'vasara'.replace('a','-').replace('a', '-');
+
+// replaceAll
+console.log('\nreplaceAll')
+console.log('vasara'.replaceAll('a','-'));
+
+console.log('spausdintinu simboliu')
+const hi = 'Labas rytas, Lietuva!';
+const x= hi.replace(' ', '');
+console.log(x.length);
+
+console.log('\ntoUpperCase');
+console.log('Labas'.toUpperCase());
+
+console.log('\ntoLowerCase');
+console.log('LAbas'.toLowerCase());
+
+// slice
+console.log('\nslice');
+console.log('pomidoras'.slice());
+console.log('pomidoras'.slice(3));
+console.log('pomidoras'.slice(0, 3));
+console.log('pomidoras'.slice(3, -3));
+console.log('pomidoras'.slice(-3));
