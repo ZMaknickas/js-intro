1666be5c2fc0aba4204978feb3c92db695c08c06
Muncle <zilvism@gmail.com>
2025-04-04 10:17:49 +0300

    initial commit

--- js/uzduotys/miestai.js
+++ js/uzduotys/miestai.js
@@ -3,21 +3,25 @@ atstumai iki Vilniaus ([‘Kaunas’, 89.6, ‘Klaipėda’, 307.3, … ).
         a. Parašykite funkciją, kuri pasakytų, kur toliausiai nuvažiuotume važiuodami x
 km/h greičiu per y valandų.
 */
-
+console.log('\nuzduotis 11111111111');
         
 const kelias = ['Marijampole', 130, 'Utena', 100, 'Druskininkai', 137, 'Kaunas', 89, 'Molėtai', 66, 'Palanga', 339, 'Birzai',
       212, 'Trakai', 30, 'Ignalina', 117, 'Rudamina', 19];
-function kelione(miestas, atstumas, laikas) {
-    let x = 80;
-    let kelione = x*laikas;    
-    if (kelione>=atstumas)
-        return miestas;
-    else 
-    return 'kitur nenuvaziuosime';
+function kelione(greitis, laikas) {
+    let maxA=0;
+    let kelione = greitis*laikas;    
+    for (let i=0; i<kelias.length; i+=2) 
+    {
+        if (kelione>=kelias[i+1] && kelione>maxA)
+        {
+        kelione=maxA;
+        x=i;
+        }
+    }    
+     console.log(kelias[x]);
 }
+kelione(80, 2)
 
-for (let i=0; i<=19; i+=2) {
-console.log(kelione(kelias[i], kelias[i+1], 1.5))}
 console.log('---------------')
 //Parašykite funkciją, kuri pasakytų, ar spėtume pasiekti norimą miestą per x
 //valandų ir kokiu greičiu turėtume važiuoti? (padarykite limitą 120 km/h)
