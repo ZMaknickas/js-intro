bb08e60e891b4971d1b901b7c440c67bc3e4fb3d
Muncle <zilvism@gmail.com>
2025-04-08 18:25:39 +0300

    initial commit

--- js/getsum.js
+++ js/getsum.js
@@ -1,13 +1,8 @@
-function people(x){
-let sum1 =0;
-let sum2 =0;
-const peoplein= x.map(peo=> peo[0]);
-const peopleout= x.map(pe=> pe[1]);
-for (let i=0; i<peoplein.length; i++) {
-sum1 +=peoplein[i]
-sum2 +=peopleout[i]}
-const number = sum1-sum2;
-return number;
-}
-
-  console.log(people([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]));
+function removeEveryOther(arr){
+  result=[];
+  for (let i=0; i<arr.length; i+=2){
+  result.push(arr[i])}
+  return result;
+  }
+  
+console.log(removeEveryOther(['x', 'o', 'x']))
