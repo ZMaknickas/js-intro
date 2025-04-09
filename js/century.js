935f6510dd8d4355a31923391c8dc7d46004023d
Muncle <zilvism@gmail.com>
2025-04-09 18:32:20 +0300

    initial commit

--- js/century.js
+++ js/century.js
@@ -1,15 +1,14 @@
 function century(year) {
 let sum=1;
-if (year<101)
+if (year<101 && year===0)
     {
     return 0;
     } 
 for (let i=101; i<=year; i+=100) 
 {
-console.log(i)    
 sum+=1
 }
 return sum;
 }
 
-console.log(century(1305))
+console.log(century(2001))
