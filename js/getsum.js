fb2d08bfd60bab632bc8e99e7708fed409b18355
webz <zilvism@gmail.com>
2025-04-10 11:20:15 +0300

    initial commit

--- js/getsum.js
+++ js/getsum.js
@@ -1,12 +1,13 @@
-function difference(ages) {
-let res= [];
-let did= Math.max(...ages);
-res.push(did);
-let maz= Math.min(...ages);
-res.push(maz);
-let dif=did-maz;
-res.push(dif);
-return res;
-}
-
-console.log(difference([3, 41, 45, 68, 70]));
+function noBoringZeros(n) {
+    let count=0;
+    if (n===0)
+    {
+      return 0;
+    } 
+    for (let i=1; i<=n; i+=10)
+    {
+        if (n%10===0)
+        count+=1
+    return s;
+    }
+console.log(noBoringZeros(2100))
